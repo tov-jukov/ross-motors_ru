cd950f064e36f2b51b98ad141d99a1c06a8e9632
Тов.Жюков <tov-jukof@ya.ru>
2017-11-09 22:03:13 +0500

    Слайдер и правка фонов

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,18 @@
-$(function() {
-
-	// Custom JS
-
-});
+new Vue({
+  el: '#vueapp',
+  data: {
+    slides: [{
+      url: 'img/licence/license-01.jpg'
+    }, {
+      url: 'img/licence/license-02.jpg'
+    }, {
+      url: 'img/licence/license-03.jpg'
+    }, {
+      url: 'img/licence/license-04.jpg'
+    }]
+  },
+components: {
+    'carousel-3d': Carousel3d.Carousel3d,
+    'slide': Carousel3d.Slide
+  }
+})
--- gulpfile.js
+++ gulpfile.js
@@ -28,6 +28,8 @@ gulp.task('common-js', function() {
 gulp.task('js', ['common-js'], function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
+		'app/libs/vue/vue.js',
+		'app/libs/vue/vue-carousel-3d.min.js',
 		'app/js/common.min.js', // Всегда в конце
 		])
 	.pipe(concat('scripts.min.js'))
