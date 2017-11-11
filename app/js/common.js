e31f34e58883deed291fb7838f23b67d0c1d5ec8
Тов.Жюков <tov-jukof@ya.ru>
2017-11-11 14:22:46 +0500

    #1
    Подвал

--- app/js/common.js
+++ app/js/common.js
@@ -1,15 +1,23 @@
 new Vue({
   el: '#vueapp',
   data: {
-    slides: [{
+    slides: [
+    {
       url: 'img/licence/license-01.jpg'
-    }, {
+    },
+
+    {
       url: 'img/licence/license-02.jpg'
-    }, {
+    },
+
+    {
       url: 'img/licence/license-03.jpg'
-    }, {
+    },
+
+    {
       url: 'img/licence/license-04.jpg'
-    }]
+    }
+    ]
   },
 components: {
     'carousel-3d': Carousel3d.Carousel3d,
