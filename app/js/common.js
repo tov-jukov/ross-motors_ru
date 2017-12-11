7f918d7a920bffa0e2d5bd01a1d77e3f43f4bea4
JoneJK <fablk@mail.ru>
2017-12-11 18:34:28 +0500

    Revert "Валидация"
    This reverts commit 1120818b6d6a3928bbd85524f5aad14d8a6434da.

--- app/js/common.js
+++ app/js/common.js
@@ -44,8 +44,7 @@ Vue.use(VueMask.VueMaskPlugin);
 Vue.directive('mask', VueMask.VueMaskDirective);
   // Validation
 
-//ad = validators.required;
-//console.log(ad);
+
 // Функция уведомления
 // options - настройка уведомления поумолчанию
 // ссылка на Noty https://ned.im/noty/#/options и https://ned.im/noty/v2/themes.html
@@ -74,11 +73,9 @@ const validationTree = validate(
 
 console.log(JSON.stringify(validationTree, null, 2))
 */
-
 //Vue.use(window.vuelidate.default);
-
+//const { required, minLength, sameAs } = window.validators;
 //----------------------------------------------------------
-
 
 //??????????? ??????????
   Vue.component('v-dialog', {
@@ -341,9 +338,7 @@ new Vue({el:"#form-calculation-to"});*/
 //project_name - название проекта
 //admin_email - адрес приёма корреспонденции
 //form_subject - форма отправитель
-SimpleVueValidation = SimpleVueValidator;
-var Validator = SimpleVueValidation.Validator;
- Vue.use(SimpleVueValidation);
+
 //const { required, minLength } = require('libs/vue/validators.js')
 Vue.component("callback", {
   template:"#callback-template",
@@ -355,42 +350,23 @@ Vue.component("callback", {
             IMail:"",
             form_subject: "Форма заказать звонок",
             chekedBS: false
-          }
+          },
+          old_form:Object.assign({}, this.form)
         }
   },
     methods:{
       onClick:function(){
-
-        parent = this;
-          this.$validate()
-          .then(function(success){
-            if(success == true){
-            parent.$parent.$emit('event');
-            var json_data = JSON.parse(JSON.stringify(parent.form));
-            json_data.project_name=init_form_data.project_name;
-            console.log(json_data);
-            axios.post('mail.php',json_data)
-            .then(function(response){console.log('success');console.log(response);NotyF({type:'success',text:"Запрос отправлен."});})
-            .catch(function(e){console.log(e);NotyF({type:'error',text:"Ошибка."});});
-            parent.reset();
-            }
-        });
-      },
-      reset:function(){
-        //old_form:Object.assign({}, this.form)
-        this.form.IName="";
-        this.form.IPhone="";
-        this.form.IMail="";
-        this.form.form_subject= "Форма заказать звонок";
-        this.form.chekedBS= false;
-        this.validation.reset();
-      }
-  },
-  validators:{
-      'form.IPhone':function(value){
-        return Validator.value(value).required().regex('[0-9]+');
+        this.$parent.$emit('event');
+        var json_data = JSON.parse(JSON.stringify(this.form));
+        json_data.project_name=init_form_data.project_name;
+        console.log(json_data);
+        axios.post('mail.php',json_data)
+        .then(function(response){console.log('success');console.log(response);NotyF({type:'success',text:"Запрос отправлен."});})
+        .catch(function(e){console.log(e);NotyF({type:'error',text:"Ошибка."});});
+        this.form = Object.assign({}, this.old_form);
       }
   }
+
 });
 new Vue({
             el: "#app-callback1",
@@ -417,46 +393,22 @@ Vue.component("calculation-locksmith-repair", {
             IMessage: "",
             form_subject: 'Форма "ОН-ЛАЙН РАСЧЕТ СТОИМОСТИ СЛЕСАРНОГО РЕМОНТА"',
             chekedBS: false
-          }
+          },
+          old_form:Object.assign({}, this.form)
         }
   },
     methods:{
       onClick:function(){
-        parent = this;
-        this.$validate()
-        .then(function(success){
-          if(success == true){
-            parent.$parent.$emit('event');
-            var json_data = JSON.parse(JSON.stringify(parent.form));
-            json_data.project_name=init_form_data.project_name;
-            console.log(json_data);
-            axios.post('mail.php',json_data)
-            .then(function(response){console.log('success');console.log(response);NotyF({type:'success',text:"Запрос отправлен."});})
-            .catch(function(e){console.log(e);NotyF({type:'error',text:"Ошибка."});});
-            parent.reset();
-            }
-        });
-      },
-      reset:function(){
-        this.form.IName = "";
-        this.form.IPhone = "";
-        this.form.IMessage = "";
-        this.form.form_subject = 'Форма "ОН-ЛАЙН РАСЧЕТ СТОИМОСТИ СЛЕСАРНОГО РЕМОНТА"';
-        this.form.chekedBS = false;
-        this.validation.reset();
+        this.$parent.$emit('event');
+        var json_data = JSON.parse(JSON.stringify(this.form));
+        json_data.project_name=init_form_data.project_name;
+        console.log(json_data);
+        axios.post('mail.php',json_data)
+        .then(function(response){console.log('success');console.log(response);NotyF({type:'success',text:"Запрос отправлен."});})
+        .catch(function(e){console.log(e);NotyF({type:'error',text:"Ошибка."});});
+        this.form = Object.assign({}, this.old_form);
       }
-    },
-    validators:{
-      'form.IName':function(value){
-      return Validator.value(value).required();
-      },
-      'form.IPhone':function(value){
-        return Validator.value(value).required().regex('[0-9]+');
-      },
-      'form.IMessage':function(value){
-        return Validator.value(value).required();
-      }
-    }
+  }
 
 });
 new Vue({
@@ -477,45 +429,21 @@ Vue.component("calculation-zip-warehouse", {
             IMessage: "",
             form_subject: 'Форма "ОНЛАЙН РАСЧЕТ СТОИМОСТИ ЗАПЧАСТЕЙ И НАЛИЧИЕ НА СКЛАДЕ"',
             chekedBS: false
-          }
+          },
+          old_form:Object.assign({}, this.form)
         }
   },
     methods:{
       onClick:function(){
-        parent = this;
-        this.$validate()
-        .then(function(success){
-            if(success == true){
-              parent.$parent.$emit('event');
-              var json_data = JSON.parse(JSON.stringify(parent.form));
-              json_data.project_name=init_form_data.project_name;
-              axios.post('mail.php',json_data)
-              .then(function(response){console.log('success');console.log(response);NotyF({type:'success',text:"Запрос отправлен."});})
-              .catch(function(e){console.log(e);NotyF({type:'error',text:"Ошибка."});});
-              parent.reset();
-          }
-        });
-  },
-  reset:function(){
-    this.form.IName = "";
-    this.form.IPhone = "";
-    this.form.IVINcode = "";
-    this.form.IMessage =  "";
-    this.form.chekedBS =  false;
-    this.form_subject = 'Форма "ОНЛАЙН РАСЧЕТ СТОИМОСТИ ЗАПЧАСТЕЙ И НАЛИЧИЕ НА СКЛАДЕ"';
-    this.validation.reset();
-  }
-},
-  validators:{
-    'form.IName':function(value){
-    return Validator.value(value).required();
-    },
-    'form.IPhone':function(value){
-      return Validator.value(value).required().regex('[0-9]+');
-    },
-    'form.IMessage':function(value){
-      return Validator.value(value).required();
-    }
+        this.$parent.$emit('event');
+        var json_data = JSON.parse(JSON.stringify(this.form));
+        json_data.project_name=init_form_data.project_name;
+        console.log(json_data);
+        axios.post('mail.php',json_data)
+        .then(function(response){console.log('success');console.log(response);NotyF({type:'success',text:"Запрос отправлен."});})
+        .catch(function(e){console.log(e);NotyF({type:'error',text:"Ошибка."});});
+        this.form = Object.assign({}, this.old_form);
+      }
   }
 
 });
@@ -537,49 +465,26 @@ Vue.component("calculation-bodywork", {
                 FILES:{},
                 form_subject: 'Форма "ОН-ЛАЙН РАСЧЕТ СТОИМОСТИ КУЗОВНОГО РЕМОНТА"',
                 chekedBS: false
-              }
+              },
+              old_form:Object.assign({}, this.form)
         }
   },
       methods:{
               onClick: function(e) {
-                parent = this;
-                this.$validate()
-                .then(function(success){
-                if(success == true){
-                parent.$parent.$emit('event');
-                var files = parent.$refs.fileInputs.files;
-                var json_data = JSON.parse(JSON.stringify(parent.form));
+                this.$parent.$emit('event');
+                var files = this.$refs.fileInputs.files;
+                /*console.log(files);*/
+                var json_data = JSON.parse(JSON.stringify(this.form));
                 json_data.project_name=init_form_data.project_name;
                 json_data.FILES = files;
+                console.log(json_data);
                 axios.post('mail.php',json_data)
                 .then(function(response){console.log('success');console.log(response);NotyF({type:'success',text:"Запрос отправлен."});})
                 .catch(function(e){console.log(e);NotyF({type:'error',text:"Ошибка."});});
-                parent.reset();//this.form = Object.assign({}, this.old_form);
-              }
-            });
-          },
-          reset:function(){
-            this.form.IName = "";
-            this.form.IPhone = "";
-            this.form.IMail = "";
-            this.form.IMessage = "";
-            this.form.FILES = {};
-            this.form.form_subject =  'Форма "ОН-ЛАЙН РАСЧЕТ СТОИМОСТИ КУЗОВНОГО РЕМОНТА"';
-            this.form.chekedBS =  false;
-            this.validation.reset();
-          }
-        },
-          validators:{
-            'form.IName':function(value){
-            return Validator.value(value).required();
-            },
-            'form.IPhone':function(value){
-              return Validator.value(value).required().regex('[0-9]+');
-            },
-            'form.IMail':function(value){
-              return Validator.value(value).email();
+                this.form = Object.assign({}, this.old_form);
+
+                }
             }
-          }
 });
 new Vue({el:"#form-calculation-bodywork"});
 /*
